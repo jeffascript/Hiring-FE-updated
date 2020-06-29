d22ce677b1a45497e9e65be2e90a9500bde8de17
jeffascript <ofoaro.jeff@gmail.com>
2020-06-29 11:16:43 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
-import "../src/styles/style.scss";
-import "./index.css";
-import Main from "../src/components/Main";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'mdbreact/dist/css/mdb.css';
+import '../src/styles/style.scss'
+import './index.css';
+import Main from '../src/components/Main'
 
-import "antd/dist/antd.css";
-import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import configureStore from "./store";
+import 'antd/dist/antd.css';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    {/* <React.StrictMode> */}
+  <React.StrictMode>
     <Main />
-    {/* </React.StrictMode> */}
-  </Provider>,
-  document.getElementById("root")
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
