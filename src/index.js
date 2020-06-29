edb63814475176d99049197a29f7fe15d7402dd7
Rushita <rushi.devi@gmail.com>
2020-06-29 13:38:55 +0200

    initialization

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import "bootstrap/dist/css/bootstrap.min.css"
-import 'mdbreact/dist/css/mdb.css';
+//import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'mdbreact/dist/css/mdb.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+//import "bootstrap/dist/css/bootstrap.min.css"
+//import 'mdbreact/dist/css/mdb.css';
 import '../src/styles/style.scss'
 import './index.css';
 import Main from '../src/components/Main'
